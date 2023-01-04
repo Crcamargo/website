ff702489aae874de2f5e80ac428e26aaaf7914c6
Cristian Camargo <crcamargo@proton.me>
2023-01-04 13:19:12 -0800

    better hello

--- components/Hello.js
+++ components/Hello.js
@@ -21,17 +21,8 @@ const Hello = () => (
           width={IMAGE_SIZE}
         />
         <div className={styles.description}>
-          I am an experienced software engineer specializing in building
-          back-end, multi-cloud, microservices.
-          <p>
-            Currently, I work as a ML Engineer focusing on deploying and
-            maintaining machine learning models in production that serve
-            millions of users 🧑🏽‍💻.
-          </p>
-          {/* <p>
-            Currently, I work on building large, complex, applications that
-            serve thousands of enterprise customers.
-          </p> */}
+          I am a ML Engineer focusing on deploying and maintaining machine
+          learning models in production that serve millions of users 🧑🏽‍💻.
         </div>
       </div>
       <Contacts />
